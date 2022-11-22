2350f1946f34b8ee29b221121fd536c656875aad
HuHuY20 <69859993+HuHuY20@users.noreply.github.com>
2022-11-22 03:38:00 -0800

    Update videos.js

--- videos.js
+++ videos.js
@@ -1,62 +1,137 @@
-
 let Videos = []
+
 let Viesos_used = []
 
-for (let index = 0; index < 5; index++) {
-    Videos.push({'src':`fds${index}`,'ID':index});
+var  Id_v = 1;
+var running_now = false;
+var running_now_id = 1;
+
+for (let index = 2; index < 27; index++) {
+    Videos.push({'src':`V${index}`,'ID':index});
 }
+
 console.log(Videos)
+
+
+
 function Get_vidos(){
-    GET_VIDOS = 0;
-    while (GET_VIDOS == 0 || GET_VIDOS == 1 ) {
+
+
+    while(true){
         Random_ummber = Math.floor(Math.random() *  Videos.length)
-        console.log(Random_ummber)
-        if(! Viesos_used.includes(Videos[Random_ummber])){
+        if(! Viesos_used.includes(Videos[Random_ummber]))
+         {
             Viesos_used.push(Videos[Random_ummber])
             break
-        } if (Videos.length  == Viesos_used.length || Videos.length -1  == Viesos_used.length ) {
-            break
-            
+
         }
+
     }
-    console.log(Viesos_used)
+    return Videos[Random_ummber]
 }
+
+
+
+function Go_to_new_Viedeos(id){
+    location.href = `#vidroRemo${id}`
+    console.log(`#vidroRemo${id}`)
+
+}
+function create_a_viedeos(){
+    var Vedios = Get_vidos()
+    console.log(Vedios['ID'])
+    let VIeDeos_Div = document.querySelector('.audio-mp3-qurran')
+    let div = document.createElement('div')
+    div.setAttribute('class','vide0Scroll')
+    let vido = document.createElement('video')
+    vido.setAttribute('id',`vidroRemo${Id_v + 1}`)  
+    vido.setAttribute('class','vidaoDsi')
+    vido.setAttribute('controlsList','noplaybackrate nodownload')
+    vido.setAttribute('autoplay','autoplay')
+    let sourse = document.createElement('sourse')
+    sourse.setAttribute('src',`/videos/V${Vedios['ID']}.mp4`)
+    sourse.setAttribute('type','videos/mp4')
+    vido.src = `/videos/V${Vedios['ID']}.mp4`
+    vido.setAttribute("controls","controls") 
+    console.log('YAMAN')
+    vido.appendChild(sourse)
+    div.appendChild(vido)
+    VIeDeos_Div.appendChild(div)
+    setTimeout(function(){
+        last_vedeos = document.querySelector(`#vidroRemo${Id_v}`).pause()
+        Go_to_new_Viedeos(Id_v + 1)
+        Id_v =  Id_v + 1
+        
+    
+    },
+    500
+    ) 
+    
+
+}
+
 
 document.addEventListener('keydown', (event) => {
     if (event.key == "ArrowDown") {
+        if (running_now || running_now_id != Id_v ) {
 
-        
-        Get_vidos()
-        Get_vidos()
-        console.log(Viesos_used[Viesos_used.length - 1])
-        let VIeDeos_Div = document.querySelector('.audio-mp3-qurran')
-        let div = document.createElement('div')
-        div.setAttribute('class','vide0Scroll')
-        let vido = document.createElement('video')
-        vido.setAttribute('id',`vidroRemo${Viesos_used[Viesos_used.length - 1]["ID"]}`)  
-        vido.setAttribute('class','vidaoDsi')
-        vido.setAttribute('controlsList','noplaybackrate nodownload')
-        let sourse = document.createElement('sourse')
-        // sourse.setAttribute('src',`/videos/fds${Viesos_used[Viesos_used.length - 1]['ID']}.mp4`)
-        sourse.setAttribute('src',`https://i.top4top.io/m_2515ni0kj1.mp4`)
-        sourse.setAttribute('type','video/mp4')
-        vido.appendChild(sourse)
-        div.appendChild(vido)
-        VIeDeos_Div.appendChild(div)
+            Go_to_new_Viedeos(running_now_id +1)
+            
+            running_now = false
+            running_now_id = running_now_id +1
+            that_vedeos = document.querySelector(`#vidroRemo${running_now_id}`) 
+            that_vedeos.play()
+            that_vedeos.currentTime = 0
 
-     
+
+            
+        } else{
+            create_a_viedeos() 
+            running_now_id = running_now_id +1
+
+        }
+
         
     
     
     } else if(event.key == "ArrowUp"){
-        console.log('YAMAN')
+        if (running_now_id != 1) {
+
+            setTimeout(function(){
+                last_vedeos = document.querySelector(`#vidroRemo${running_now_id}`).pause()
+                running_now_id = running_now_id -1
+                that_vedeos = document.querySelector(`#vidroRemo${running_now_id}`)
+                that_vedeos.play()
+                that_vedeos.currentTime = 0
+                Go_to_new_Viedeos(running_now_id)
+                running_now = true
+                
+            
+            },
+            500
+            ) 
+            
+        }
+
     }
 
 }); window.onwheel = e => {
-  if(e.deltaY >= 0){
-    console.log('Scroll Down');
+  if(e.deltaY >= 133){
+    create_a_viedeos()
+    setTimeout(function(){
+        console.log('slepp')
+        Go_to_new_Viedeos(Id_v + 1)
+        Id_v =  Id_v + 1
+    },
+    500)
+
+
+
   } else {
 
     console.log('Scroll Up');
   }
 }
+
+// 
+
